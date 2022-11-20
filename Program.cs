5b8a1e20daf0f3176ab1e7107c8690147efb26e9
Rob <Rmilla445@gmail.com>
2022-11-20 10:11:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CallOfDuty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
